5989a771c878522a58dec358c4fba906ffd59aa8
bansoderocker <59302067+bansoderocker@users.noreply.github.com>
2022-07-21 09:40:02 +0530

    Add files via upload

---
+++ 20 july 2022/MaxMinValue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinMaxValue
+{
+    internal class MaxMinValue
+    {
+        public int Max { get; set; }
+        public int Min { get; set; }
+
+
+        public MaxMinValue() { }
+        public MaxMinValue(int[] myArray)
+        {
+
+            GetMax(myArray);
+            GetMin(myArray);
+        }
+
+        public void GetMax(int[] myArray)
+        {
+            Max = myArray[0];
+            foreach (int i in myArray)
+            {
+                if (i > Max) { Max = i; }
+            }
+
+
+        }
+        public void GetMin(int[] myArray)
+        {
+            Min = myArray[0];
+            foreach (int i in myArray)
+            {
+                if (i < Min) { Min = i; }
+            }
+
+
+        }
+    }
+}
