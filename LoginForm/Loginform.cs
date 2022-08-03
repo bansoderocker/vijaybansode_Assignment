e8d8c7b6f4e8f4318f7af627aef51d466db66c0a
bansoderocker <59302067+bansoderocker@users.noreply.github.com>
2022-08-03 08:36:58 +0530

    Update Loginform.cs

--- LoginForm/Loginform.cs
+++ LoginForm/Loginform.cs
@@ -23,20 +23,43 @@ namespace WindowsFormsAppDB
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            USERDATA user = new USERDATA();
+            VerifyUser(1);
+        }  
 
+        private void btnLoginDisConn_Click(object sender, EventArgs e)
+        {
+            VerifyUser(2);
+        }
+
+        void VerifyUser(int a)
+        {
+
+            USERDATA user = new USERDATA();
+            bool result_flag = false;
             try
             {
                 user.Username = txtUsername.Text;
                 user.Password = txtPassword.Text;
-                if (empDataSource.ValidateUser(user))
+
+                if (a == 1)
+                {
+                    result_flag = empDataSource.ValidateUser(user);
+                }
+                else {
+                    result_flag = empDataSource.ValidateUserDisConn(user);
+                }
+
+                if (result_flag)
                 {
                     MessageBox.Show("Login Successful..");
                     this.Hide();
                     DataDML page2 = new DataDML();
                     page2.Show();
                 }
-                else { MessageBox.Show("Login Failed..!"); }
+                else
+                {
+                    MessageBox.Show("Login Failed..!");
+                }
 
             }
             catch (Exception ex)
@@ -46,7 +69,8 @@ namespace WindowsFormsAppDB
             finally { clearTextbox(); }
         }
 
-        void clearTextbox() {
+        void clearTextbox()
+        {
             txtPassword.Text = String.Empty;
             txtUsername.Text = String.Empty;
         }
